6cf8a6d09b37a041824b4b7bcc69dd1f4146739c
Zhang Yu <feiyuw@gmail.com>
2015-11-26 09:05:20 +0800

    fix len is 0 bug

--- pullstream.js
+++ pullstream.js
@@ -61,14 +61,11 @@ PullStream.prototype.pullUpTo = over([
 
 PullStream.prototype.pipe = over([
   [over.numberOptionalWithDefault(null), over.object, function (len, destStream) {
-    if (!len) {
+    if (len === null) {
       return PassThrough.prototype.pipe.call(this, destStream);
-    }
-
-    if (len === 0) {
+    } else if (len === 0) {
       return destStream.end();
     }
-
 
     var pullstream = this;
     pullstream
